75546440b961a9fa1897d3fdf30acf3f7459a020
Jonathan Foster <jon.foster.ny@gmail.com>
2024-01-19 07:23:39 -0500

    downcase the dog name

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,6 @@
 class DogsController < ApplicationController
   def fetch_dog
-    @breed_name = params[:breed]
+    @breed_name = params[:breed]&.downcase
     @dog_image_url = DogService.new(@breed_name).fetch_dog
     render partial: 'dog'
   end
