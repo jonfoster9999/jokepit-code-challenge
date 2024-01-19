ee72769064c7a97b06f66cf259f13c7e75653af2
Jonathan Foster <jon.foster.ny@gmail.com>
2024-01-18 21:15:20 -0500

    small refactor to dog service

--- app/services/dog_service.rb
+++ app/services/dog_service.rb
@@ -1,19 +1,25 @@
 require 'net/http'
 class DogService
   API_URL = "https://dog.ceo/api/breed/%s/images"
-
-  attr_accessor :search_term
-  def initialize(search_term)
-    @search_term = search_term
+  attr_accessor :breed
+  def initialize(breed)
+    @breed = breed
   end
+
   def fetch_dog
-    uri = URI(API_URL % search_term)
-    res = Net::HTTP.get_response(uri)
+    res = Net::HTTP.get_response(search_uri)
     return nil unless res.is_a?(Net::HTTPSuccess)
     body = JSON.parse(res.body)
     msg = body['message']
+    # take a random dog image from the array.
     msg&.sample
   rescue
     nil
+  end
+
+  private
+
+  def search_uri
+    @search_uri ||= URI(API_URL % breed)
   end
 end
